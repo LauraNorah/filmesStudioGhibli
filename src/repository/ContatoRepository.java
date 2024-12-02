02eed69a8a280d34567a5549ade0b1273691581e
LauraNorah <laura.nora.azevedo@gmail.com>
2024-12-02 03:32:47 -0300

    primeiro commit

---
+++ src/repository/ContatoRepository.java
@@ -0,0 +1,19 @@
+package repository;
+
+import java.util.List;
+
+import models.Contato;
+
+public class ContatoRepository {
+
+    public List<Contato> obterTodosContatos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'obterTodosContatos'");
+    }
+
+    public void adicionarContato(int selectedId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'adicionarContato'");
+    }
+    
+}
---
+++ src/views/ContatoForm.java
@@ -0,0 +1,21 @@
+package views;
+
+import models.Contato;
+
+public class ContatoForm {
+
+    public ContatoForm(ContatoTableView tableView, String string) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public Contato getContato() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getContato'");
+    }
+
+    public void setVisible(boolean b) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setVisible'");
+    }
+    
+}
